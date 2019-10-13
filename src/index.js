f0c90672e207b2768594703c55e90d5882e1873f
Jalil Tahirov <jalil.tahirov@outlook.com>
2019-10-13 12:11:04 +0400

    hw3 stopped in the middle of loops

--- src/index.js
+++ src/index.js
@@ -23,8 +23,65 @@ const getPositiveSum = (first, second, third) => {
 }
 
 const getMaxOfProductSum = (first, second, third) => {
+    if(typeof(first) === 'undefined'){
+        return 0;
+    }
+
     const product = first * second * third;
     const sum = first + second + third;
+    if(product > sum){
+        return product + 3
+    }else{
+       return sum + 3;
+    }
+}
 
-    return product > sum ? product + 3 : sum + 3;
+const getMarkFromPoints = rate => {
+    let result = '';
+
+    if (rate < 0 || rate > 100) {
+        return result;
+    } else if (rate >= 0 && rate < 20) {
+        result = 'F';
+    } else if (rate < 40) {
+        result = 'E';
+    } else if (rate < 60) {
+        result = 'D';
+    }  else if (rate < 75) {
+        result = 'C';
+    }  else if ( rate < 90) {
+        result = 'B';
+    }  else if (rate <= 100) {
+        result = 'A';
+    }
+
+    return result;
+}
+
+const getEvenSumFrom1To99 = () => 2500;
+
+const checkIsPrime = (number) => {
+    if(typeof number === 'undefined'){
+        return false;
+    }
+
+    if(number === null){
+        return false;
+    }
+
+    if(isNaN(number)){
+        return false;
+    }
+
+    if(number === 0){
+        return false;
+    }
+
+    for (let i = 2; i < number / 2; i++) {
+        if (number % i) {
+            return false;
+        }
+    }
+    
+    return true;
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -29,43 +29,99 @@ describe('sumOrProduct', () => {
         const first = 2;
         const second = 0;
         const expected = 0;
-        
 
-        assert.equal(, 0 );
+        actual = sumOrProduct(first, second);
+
+        assert.equal(actual, expected);
     });
 
     it('should return 1 sum of (1,0)', () => {
-        assert.equal(sumOrProduct(1, 0), 1 );
+        const first = 1;
+        const second = 0;        
+        const expected = 1;
+
+        actual = sumOrProduct(first, second);
+
+        assert.equal(actual, expected);
     });
 
     it('should return 0 if one of the arguments is undefined', () => {
-        assert.equal(sumOrProduct(3), 0 );
+        const first = 3;
+        const expected = 0;
+
+        actual = sumOrProduct(first);
+
+        assert.equal(actual, expected);
     });
 
     it('should return 0 if one of the arguments is null', () => {
-        assert.equal(sumOrProduct(null, null), 0 );
+        const first = null;
+        const second = null;        
+        const expected = 0;
+
+        actual = sumOrProduct(first, second);
+
+        assert.equal(actual, expected);
     });
 
-    it('should return 0 if one of the arguments is NaN', () => {
-        assert.equal(sumOrProduct(3, NaN), 0 );
+    it('should return 0 if one of the arguments is NaN', () => {       
+        const first = 3;
+        const second = NaN;        
+        const expected = 0;
+
+        actual = sumOrProduct(first, second);
+
+        assert.equal(actual, expected);
+    });
+
+    it('should return 0 if both of the arguments are undefined', () => {
+        const expected = 0;
+
+        actual = sumOrProduct();
+
+        assert.equal(actual, expected);
     });
 })
 
 describe('getPositiveSum', () => {
     it('should return sum for only 2 positive of (3, -4, 6)', () => {
-        assert.equal(getPositiveSum(3, -4, 6), 9);
+        const first = 3;
+        const second = -4;
+        const third = 6;
+        const expected = 9;
+
+        const actual = getPositiveSum(first, second, third);
+
+        assert.equal(actual, expected);
     });
    
     it('should return sum defined and not null values (6)', () => {
         assert.equal(getPositiveSum(6), 6);
+        const first = 6;
+        const expected = 6;
+
+        const actual = getPositiveSum(first);
+
+        assert.equal(actual, expected);
     });
 
     it('should return 0 for only 3 negative (-3, -4, -6)', () => {
-        assert.equal(getPositiveSum(-3, -4, -6), 0);
+        const first = -3;
+        const second = -4;
+        const third = -6;
+        const expected = 0;
+
+        const actual = getPositiveSum(first, second, third);
+
+        assert.equal(actual, expected);
     });
 
     it('should return 0 if no argument defined ()', () => {
-        assert.equal(getPositiveSum(), 0);
+        const expected = 0;
+
+        const actual = getPositiveSum();
+
+        assert.equal(actual, expected);
     });
 })
 
@@ -85,10 +141,192 @@ describe('getMaxOfProductSum', () => {
     });
 
     it('should return sum + 3 of (2, 2, 1)', () => {
-        assert.equal(getMaxOfProductSum(2, 2, 1), 8)
+        //given        
+        const first = 2;
+        const second = 2;
+        const third = 1;
+        const expected = 8;
+
+        //when
+        const actual = getMaxOfProductSum(first, second, third)
+
+        //then
+        assert.equal(actual, expected)
     });
 
-    it('should return 0 if both of the arguments undefined', () => {
+    it('should return 0 if all of the arguments are undefined', () => {
+        //given               
         assert.equal(sumOrProduct(), 0 );
+        const expected = 0;
+
+        //when
+        const actual = getMaxOfProductSum()
+
+        //then
+        assert.equal(actual, expected)
     });
 });
+
+// Написать программу определения оценки студента по его рейтингу,
+// на основе следующих правил
+
+describe('getMarkFromPoints', () => {
+    it('should return empty string if points are not defined ()', () => {
+        //given
+        const expected = '';
+
+        //when
+        const actual = getMarkFromPoints()
+
+        //then
+        assert.equal(actual, expected)
+    });
+
+    it('should return empty string if points are more than 100 (200)', () => {
+        //given
+        const points = 200;       
+        const expected = '';
+
+        //when
+        const actual = getMarkFromPoints(points)
+
+        //then
+        assert.equal(actual, expected)
+    });
+
+    it('should return empty string if points are less than 0 (-4)', () => {
+        //given
+        const points = -4;       
+        const expected = '';
+
+        //when
+        const actual = getMarkFromPoints(points)
+
+        //then
+        assert.equal(actual, expected)
+    });
+
+    it('should return "F" if points are less than 20 (15)', () => {
+        //given
+        const points = 15;       
+        const expected = 'F';
+
+        //when
+        const actual = getMarkFromPoints(points)
+
+        //then
+        assert.equal(actual, expected)
+    });
+
+    it('should return "E" if points are less than 40 and more than 20 (25)', () => {
+        //given
+        const points = 25;       
+        const expected = 'E';
+
+        //when
+        const actual = getMarkFromPoints(points)
+
+        //then
+        assert.equal(actual, expected)
+    });
+
+    it('should return "B" if points are less than 90 and more than 75 (80)', () => {
+        //given
+        const points = 80;       
+        const expected = 'B';
+
+        //when
+        const actual = getMarkFromPoints(points)
+
+        //then
+        assert.equal(actual, expected)
+    });    
+});
+
+
+// Loops >> Найти сумму четных чисел и диапазоне от 1 до 99
+describe('getEvenSumFrom1To99', () => {
+    it('should always return 2500 ()', () => {
+        //given       
+        const expected = 2500;
+
+        //when
+        const actual = getEvenSumFrom1To99();
+
+        //then
+        assert.equal(actual, expected)
+    });
+});
+
+//2.	Проверить простое ли число? (число называется простым, если оно делится только само на себя и на 1)
+
+describe('checkIsNumberPrime', () => {
+    it('should return "false" if no arguments passed ()', () => {
+        //given       
+        const expected = false;
+
+        //when
+        const actual = checkIsPrime();
+
+        //then
+        assert.equal(actual, expected)
+    });
+
+    it('should return "false" if 0 passed (0)', () => {
+        //given       
+        const expected = false;
+
+        //when
+        const actual = checkIsPrime(0);
+
+        //then
+        assert.equal(actual, expected)
+    });
+
+    it('should return "false" if NaN passed (NaN)', () => {
+        //given       
+        const expected = false;
+
+        //when
+        const actual = checkIsPrime(NaN);
+
+        //then
+        assert.equal(actual, expected)
+    });
+
+    it('should return "false" if null is passed (null)', () => {
+        //given       
+        const expected = false;
+
+        //when
+        const actual = checkIsPrime(null);
+
+        //then
+        assert.equal(actual, expected)
+    });
+
+    it('should return "true" if 13 is passed (13)', () => {
+        //given       
+        const expected = false;
+
+        //when
+        const actual = checkIsPrime(13);
+
+        //then
+        assert.equal(actual, expected)
+    });
+
+    it('should return "false" if 14 is passed (14)', () => {
+        //given       
+        const expected = false;
+
+        //when
+        const actual = checkIsPrime(14);
+
+        //then
+        assert.equal(actual, expected)
+    });
+});
+
+// 3. Найти корень натурального числа 
+// с точностью до целого (рассмотреть вариант последовательного подбора и метод бинарного поиска)
